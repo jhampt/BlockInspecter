d290af88eabc6f5a3b30d256ecb98923f94173da
jhampt <49539815+jhampt@users.noreply.github.com>
2019-05-22 11:05:07 -0400

    Delete Provider.php

--- src/BlockInspecter/Providers/Provider.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace BlockInspecter\Providers;
-use pocketmine\block\Block;
-use pocketmine\Player;
-interface Provider{
-	public function getLogsAt(Block $block) : array;
-	public function log($action, Block $block, Player $player) : void;
-	public function close() : void;
-}
